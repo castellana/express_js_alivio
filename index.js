53d9204c42c138b998991eddfaf818e6603cdd07
castellana <r_aquel@hotmail.com>
2020-12-14 23:42:25 +0100

    pages index, solutions, community, 404

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+    res.sendFile('./views/index.html', {root: __dirname})
+})
+
+app.get('/solutions', (req, res) => {
+    res.sendFile('./views/solutions.html', {root: __dirname})
+})
+
+app.get('/community', (req, res) => {
+    res.sendFile('./views/community.html', {root: __dirname})
+})
+
+app.get('/pricing', (req, res) => {
+    res.sendFile('./views/pricing.html', {root: __dirname})
+})
+
+
+
+app.use((req, res) => {
+    res.status(404).sendFile('./views/404.html', {root: __dirname})
+})
+
+app.listen(3000, () => console.log('Server running...'));
